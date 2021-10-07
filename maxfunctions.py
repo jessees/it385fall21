dc08fcdcb68e48d16b2d2f4bc226613dd0481ff2
jessees <schoenbachler.jessee@student.greenriver.edu>
2021-10-06 20:02:27 -0700

    functions

---
+++ maxfunctions.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def maxOfTwoNumber(a, b):
+    #code to do
+    #compare the 2 values
+    if a > b:
+        max = a
+    else:
+        max = b
+    return max
+
+#Return the higher of the two numbers
+def main():
+    n1 = 3
+    n2 = 4
+    maximum = maxOfTwoNumber(n1, n2)
+    print("maximum of", n1, "and",n2, "is", maximum)
+main()
